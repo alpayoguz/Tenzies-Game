07bf9e794455a9f967ef8342efa134203a5958d8
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-04-11 02:43:55 +0300

    bare-bones created

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <>
+    <div class="background">
+        <div className='main'>
+            <div className='main-inner'></div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
