82e5c0fd2d3a19a94b03614b6c79fbb995f00d8c
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-04-12 00:43:53 +0300

    diceArray state added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react';
 import './css/style.css';
-import Main from "./components/Main"
+import Dice from "./components/Dice"
+
 
 function App() {
+
+  function randomDiceArray(){
+    const diceArray = []
+    for(let i =0;i < 10; i++){
+      const singleDice = Math.floor(Math.random() * 6) + 1
+      diceArray.push(singleDice);
+    }
+    return diceArray
+  }
+
+  const [diceArray, setDiceArray] = useState(()=> randomDiceArray())
+
+  const diceComponents = diceArray.map(dice=> <Dice value ={dice}/>)
+
   return (
- <>
-  <Main/>
- </>
+<div class="container">
+  <main>
+                <div className="dice-container">
+                   {diceComponents}
+                </div>
+          </main>
+</div>
   )
 }
 
