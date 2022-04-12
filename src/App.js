628181c22eec40ef063931c88c5fa3eb02a22018
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-04-12 06:09:49 +0300

    hasWon boolean and reset game function added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import { useState, useEffect } from 'react';
 import './css/style.css';
 import Dice from "./components/Dice"
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
 
 function App() {
   const [diceArray, setDiceArray] = useState(()=> randomDiceArray())
+  const [hasWon, setHasWon] = useState(false)
+
+
+  useEffect(()=>{
+    const reservedDice = diceArray[0].value
+    const allDicesAreSame = diceArray.every(dice => dice.value === reservedDice )
+    const allDicesIsHeld = diceArray.every(dice=> dice.isHeld)
+    if(allDicesAreSame && allDicesIsHeld){
+     setHasWon(true)
+    }
+  })
+
+  function resetGame(){
+    setHasWon(false)
+    setDiceArray(randomDiceArray())
+  }
+  const diceComponents = diceArray.map(dice=> <Dice isHeld={dice.isHeld} key={dice.key} id={dice.key}  value ={dice.value} flipDice={flipDice}  />)
 
   
   function generateDice(){
-    return {key:nanoid() ,value:Math.floor(Math.random() * 6) + 1, isHeld:false}
+    return {key:nanoid() ,value:Math.floor(Math.random()*6) + 1, isHeld:false}
   }
+
 
 
   function randomDiceArray(){
@@ -26,6 +45,8 @@ function App() {
     setDiceArray(dices=> dices.map(dice=>{
        return dice.isHeld ? dice : generateDice()
     }))
+
+    hasWon && resetGame()
   }
 
 
@@ -33,7 +54,9 @@ function flipDice(id){
   setDiceArray(dices => dices.map(dice=> dice.key === id ? {...dice, isHeld:true} : dice) )
 }
 
-  const diceComponents = diceArray.map(dice=> <Dice isHeld={dice.isHeld} key={dice.key} id={dice.key}  value ={dice.value} flipDice={flipDice}  />)
+
+
+ 
 
 
   return (
@@ -42,7 +65,11 @@ function flipDice(id){
                 <div className="dice-container">
                    {diceComponents}
                 </div>
-                <button className='btn-roll' onClick={()=>{rollDice()}}>Roll</button>
+                <button className='btn-roll' onClick={()=>{rollDice()}}>{hasWon ? "Next Game" : "Roll"}</button>
+                <div className='confetti'>
+                   {hasWon && <Confetti width={1000} height={400} confettiSource={{x:3, y:0, h:0, w:100}}/>}
+                </div>
+               
           </main>
 </div>
   )
